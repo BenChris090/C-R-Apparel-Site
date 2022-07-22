4a933c503b60de8f33661b327558bdf4f8458d26
unknown <you@example.com>
2022-07-22 19:55:21 +0100

    added the dark mode toggle

--- js/index.js
+++ js/index.js
@@ -8,6 +8,23 @@ function openNav() {
     document.getElementById("mNav").style.width = "0";
   }
 
+function nMode() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("relText").style.color = "white";
+    document.getElementById("relTexts").style.color = "white";
+    document.getElementById("relTexts1").style.color = "white";
+    document.getElementById("night").style.display = "none";
+    document.getElementById("day").style.display = "flex";
+}
+
+function lMode() {
+    document.getElementById("night").style.display = "flex";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("relText").style.color = "black";
+    document.getElementById("relTexts").style.color = "black";
+    document.getElementById("relTexts1").style.color = "black";
+    document.getElementById("day").style.display = "none";
+}
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 // function openNav() {
