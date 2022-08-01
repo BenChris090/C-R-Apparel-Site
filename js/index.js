aede617f9561950ae77bc066287b7fd5e159ac7a
unknown <you@example.com>
2022-08-02 00:50:28 +0100

    login page commit

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ function openNav() {
     document.getElementById("mNav").style.width = "0";
   }
 
+// mobile functions start here
 function nMode() {
     document.body.style.backgroundColor = "black";
     document.getElementById("relText").style.color = "white";
@@ -27,6 +28,29 @@ function lMode() {
     document.getElementById("relTexts1").style.color = "black";
     document.getElementById("day").style.display = "none";
 }
+// mobile functions ends here
+
+// mobile functions start here
+function nMode() {
+  document.body.style.backgroundColor = "black";
+  document.getElementById("relText").style.color = "white";
+  document.getElementById("relTexts").style.color = "white";
+  document.getElementById("relTexts1").style.color = "white";
+  document.getElementById("nights").style.display = "none";
+  document.getElementById("days").style.display = "flex";
+  document.getElementById("main").style.boxShadow = "0px 15px 10px -15px white";
+  // 0px 15px 10px -15px #111
+}
+
+function lMode() {
+  document.getElementById("nights").style.display = "flex";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("relText").style.color = "black";
+  document.getElementById("relTexts").style.color = "black";
+  document.getElementById("relTexts1").style.color = "black";
+  document.getElementById("days").style.display = "none";
+}
+// desktop functions end here
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 // function openNav() {
